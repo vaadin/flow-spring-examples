1d229eec9443d50fb91934c2418d65a0509f6a22
Denis Anisimov <denis@vaadin.com>
2017-10-22 19:08:47 +0300

    Make initial Spring tutorial project structure
    Fix for #2573

---
+++ src/main/java/org/vaadin/spring/tutorial/TutorialApplication.java
@@ -0,0 +1,13 @@
+package org.vaadin.spring.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialApplication.class, args);
+    }
+}
+
