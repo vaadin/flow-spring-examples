46a3b388ebb1efdbfa8540fba12c7157d1393270
Denis <denis@vaadin.com>
2018-01-22 16:16:12 +0300

    Make the router link localized and provide a way to change locale (#18)
    * Make the router link localized and provide a way to change locale
    
    Fix for #3180
    
    * Add an item about I18N to the README
    
    * Merge branch 'master' into 3180-i18n-spring

--- src/main/java/org/vaadin/spring/tutorial/RootComponent.java
+++ src/main/java/org/vaadin/spring/tutorial/RootComponent.java
@@ -15,23 +15,50 @@
  */
 package org.vaadin.spring.tutorial;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.dom.Style;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 
 @Route("")
-public class RootComponent extends Div {
+public class RootComponent extends Div implements LocaleChangeObserver {
+
+    private RouterLink link;
 
     public RootComponent(@Autowired Greeter greeter,
             @Autowired ExampleTemplate template) {
         Label greeting = new Label(greeter.sayHello());
-        greeting.getElement().getStyle().set("display", "block");
-        RouterLink link = new RouterLink("Navigate to another component",
+        Style grretingStyle = greeting.getElement().getStyle();
+        grretingStyle.set("display", "block");
+        grretingStyle.set("margin-bottom", "10px");
+
+        Button button = new Button("Switch language to Chinese",
+                event -> getUI().get().setLocale(Locale.CHINESE));
+
+        link = new RouterLink(
+                getI18NProvider().getTranslation("root.navigate_to_component"),
                 ViewComponent.class);
-        add(greeting, link, template);
+
+        Style linkStyle = link.getElement().getStyle();
+        linkStyle.set("display", "block");
+        linkStyle.set("margin-bottom", "10px");
+
+        add(greeting, button, link, template);
         add(template);
     }
+
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        link.setText(
+                getI18NProvider().getTranslation("root.navigate_to_component"));
+    }
+
 }
